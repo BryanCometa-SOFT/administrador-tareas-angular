8c323c90713a6bab9c3ba95121e427d467ff1591
BryanCometa-SOFT <bscometa@misena.edu.co>
2025-03-13 18:42:10 -0500

    config: automatización del despliegue en Firebase con GitHub Actions

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyA5BXydszCX4bKHwCWv0gYzM_0YsEa_y-M",
+        authDomain: "gestor-tareas-angular-web.firebaseapp.com",
+        projectId: "gestor-tareas-angular-web",
+        storageBucket: "gestor-tareas-angular-web.firebasestorage.app",
+        messagingSenderId: "25071557283",
+        appId: "1:25071557283:web:14d3bb21596c6fd462fd16",
+        measurementId: "G-1MP93P0LVB"
+    }
+  };
